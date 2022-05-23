fa46f2377ceb9ff01aed6ac8e144b5e2f78fae7d
Kaz Karl <kaz.michael@cox.com>
2022-05-23 11:18:32 -0400

    For Gil

--- sql/MS_Users.php
+++ sql/MS_Users.php
@@ -2,7 +2,7 @@
 $windowsUser = preg_replace("/^.+\\\\/", "", $_SERVER["AUTH_USER"]);
 
 //PROJECT OWNERS
-$sql_winuser = "SELECT * FROM [EPS].[RiskandIssues_Users] WHERE Username = '$windowsUser'";
+$sql_winuser = "SELECT * FROM [RI_MGT].[RiskandIssues_Users] WHERE Username = '$windowsUser'";
 $stmt_winuser = sqlsrv_query( $data_conn, $sql_winuser ); 
 $row_winuser = sqlsrv_fetch_array( $stmt_winuser, SQLSRV_FETCH_ASSOC);
 //$row_winuser['Email']
