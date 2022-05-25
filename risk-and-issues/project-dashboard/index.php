c59486bdf0f68c67793e6eaaf71264a7ca823fc1
Kaz <kaz.michael@cox.com>
2022-05-25 16:32:34 -0400

    Kaz — Menu item name change — Symetreeeee!

--- risk-and-issues/project-dashboard/index.php
+++ risk-and-issues/project-dashboard/index.php
@@ -11,7 +11,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Program R&I Aggregate View</title>
+    <title>Project R&I Aggregate View</title>
     <link rel="shortcut icon" href="favicon.ico"/>
     <?php 
     include ("../../includes/load.php");
