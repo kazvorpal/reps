efea7bcad143500c2cd7cc98c501c19da5d4c660
Kaz Karl <kaz.michael@cox.com>
2022-06-21 16:23:44 -0400

    missed file

---
+++ risk-and-issues/includes/ri-functions.js
@@ -0,0 +1,10 @@
+function myConfirmation() {
+    let text = "If you go back, you will lose the information you have entered or changed on this form.";
+    if (confirm(text) == true) {
+      text = "You pressed OK!";
+      window.history.go(-1);
+    } else {
+      text = "You canceled!";
+    }
+    document.getElementById("demo").innerHTML = text;
+  }
