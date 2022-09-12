601cebf917b0d7cfd95695dd82bdaef6fc60e223
Kaz <kaz.michael@cox.com>
2022-09-12 09:31:34 -0400

    Kaz — Various tweaks and improvements

--- risk-and-issues/global/index.php
+++ risk-and-issues/global/index.php
@@ -837,6 +837,10 @@ const subevent = () => {
 
 const levelevent = (e) => {
   // console.log("levelevent");
+  programlevel();
+}
+
+const programlevel = () => {
   if (document.querySelector(`input[name="RILevel"]:checked`).value == "Portfolio") {
     // console.log("portfolio");
     document.getElementById("program").multiple = true;
@@ -850,6 +854,7 @@ const levelevent = (e) => {
     setsubprogramevent();
   }
 }
+
 
 // Event creators
 
@@ -919,11 +924,12 @@ $(document).ready(function() {
   document.querySelector("#date").addEventListener("keydown", (e) => {e.preventDefault()});
   document.querySelector("#DateClosed").addEventListener("keydown", (e) => {e.preventDefault()});
   disabler({t: "RIType", v: "Issue", d: ["RiskProbability", "riskRealized"], e: "risk"})
-  disabler({t: "RILevel", v: "Program", d: ["portfolioType"], e: "Portfolio"})
-  disabler({t: "RILevel", v: "Portfolio", d: ["Region[]", "subprogram"], e: "Portfolio"})
+  disabler({t: "RILevel", v: "Program", d: ["portfolioType", "TransfertoProgramManager"], e: "Portfolio"})
+  disabler({t: "RILevel", v: "Portfolio", d: ["Region[]", "subprogram"], e: "Program"})
   setregionevent();
   setsubprogramevent();
   setlevelevent();
+  programlevel();
 });
 
 </script>
