289aefde6c2cef1a278ac921e909f8b469ed0455
Kaz <kaz.michael@cox.com>
2022-10-20 09:53:02 -0400

    Kaz — Return false on click

--- risk-and-issues/dashboard/index.php
+++ risk-and-issues/dashboard/index.php
@@ -725,7 +725,7 @@
 
 
     var modebutton = (target) => {
-        let url = "<a href='" +"/risk-and-issues/dashboard/?mode=" + target + "' style='color:#fff'>";
+        let url = "<a href='" +"/risk-and-issues/dashboard/?mode=" + target + "' style='color:#fff' onclick='return false';>";
         let rest = (target == "portfolio") ? "RAID Log" : capitalize(target);
         return makeelement({"i": target + "mode", "t": url + rest + ",</a>", "e": "div", "c": "btn btn-primary ml-3","j": function() {
             console.log("changing mode to " + target);
