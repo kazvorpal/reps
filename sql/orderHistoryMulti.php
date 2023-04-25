482ab128821142538b08822f71ea3f78e6713f7f
Gilbert Carolino <Gilbert.Carolino@Cox.com>
2023-04-25 15:45:50 -0400

    EQ Fix

--- sql/orderHistoryMulti.php
+++ sql/orderHistoryMulti.php
@@ -19,8 +19,10 @@
 	}
 	
 	$sql_eqh = "Select * 
-				From OrdMgt.fn_GetOrderHistoryEquipmentId(2023)
-				WHERE  GL_Project_Num IN (SELECT convert(varchar, value) FROM string_split('$oracleCD', ','))
+				From (Select o.*, ep.EquipPlan_Id
+	    				From OrdMgt.fn_GetOrderHistory(2023) o
+	    				Left Outer Join PORMgt.EquipPlan ep on ep.EPSProject_Key = o.EPS_POR_Project_Key and ep.FiscalYear_Key = o.FiscalYear_Key) a
+				WHERE GL_Project_Num IN (SELECT convert(varchar, value) FROM string_split('$oracleCD', ','))
 							  OR Order_Num IN (SELECT convert(varchar, value) FROM string_split('$oracleCD', ','))
 							  OR Requisition_Num IN (SELECT convert(varchar, value) FROM string_split('$oracleCD', ','))
 							  OR PPM_Num IN (SELECT convert(varchar, value) FROM string_split('$oracleCD', ','))
@@ -30,7 +32,7 @@
 							  OR EquipPlan_Id IN ('$oracleCD')
 							  OR EPS_Project_Nm IN ('$oracleCD')
 				";
-				//echo $sql_eqh;
+				echo $sql_eqh;
 	$stmt_eqh = sqlsrv_query( $conn, $sql_eqh ); //$conn_COXProd is Cox Datebase on SQL Production //$conn_COX_QA is test Datebase 
 
 	// OR EPS_Project_Nm IN (SELECT convert(varchar, value) FROM string_split('$oracleCD', ','))
