eb03cac7c245d39ddb35db8607a82bd7d5134fac
Kaz <kaz.michael@cox.com>
2023-05-09 18:27:07 -0400

    Stuff

--- risk-and-issues/js/ri.js
+++ risk-and-issues/js/ri.js
@@ -311,3 +311,202 @@ const striptags = (html) =>
   return fieldswitch[test];
   }
 
+
+
+
+
+
+
+
+
+
+
+
+  // const fieldswitch = {
+  //   //    Specific fields that need extra calculation
+  //   //    Add any field to rifields that you want to be a column,
+  //   //    in the format {fieldname: "Human Name"}
+  //   //    If it exists in rifields, it will be populated automatically here.
+  //   //    If, instead, you need to do some calculation to produce it,
+  //   //    add its fieldname to this "switch" object, fieldswitch,
+  //   //    with an anonymous function to handle the changes.
+  //   RiskAndIssue_Key: () => {
+  //       return `<span style='font-weight:900'>${text}</span>`;
+  //   },
+  //   mangerlist: () => {
+  //     if (ri["MLMProgram_Key"] || mode("project")) {
+  //       const manger = mangerlist[ri["Fiscal_Year"] + "-" + ri["MLMProgram_Key"]];
+  //       let mangers = [ri["Fiscal_Year"]];
+  //       for (man of manger) {
+  //           mangers.push(man.User_Nm);
+  //       }  
+  //       return mangers.join().replace(",", ", ");
+  //     } else
+  //       return "";
+  //   },
+  //   global: () => {
+  //       return  (ri.Global_Flg) ? "Y" : "N";
+  //     },
+  //   groupcount: () => {
+  //     let gc = 0;
+  //     ridata.forEach(o => {
+  //       gc += (ri.RIIncrement_Num == o.RIIncrement_Num && ri.RIActive_Flg == o.RIActive_Flg) ? 1 : 0;
+  //     })
+  //     return gc;
+  //   },
+  //   category: () => {
+  //     return  (ri.Global_Flg) ? "Global" : "Program";
+  //   },
+  //   EPSSubprogram_Nm: () => {
+  //       return getlocationbykey(ri.EPSProject_Key)
+  //   },
+  //   ForecastedResolution_Dt: () => {
+  //       return (ri.ForecastedResolution_Dt == null) ? "Unknown" : makestringdate(ri.ForecastedResolution_Dt);
+  //   },
+  //   RIActive_Flg: () => {
+  //       return (ri.RIActive_Flg) ? "Open" : "Closed";
+  //   },
+  //   Created_Ts: () => {
+  //       return makestringdate(ri.Created_Ts);
+  //   },
+  //   monthcreated: () => {
+  //       return new Date(ri.Created_Ts.date).toLocaleString('default', { month: 'long' });
+  //   },
+  //   monthclosed: () => {
+  //       return (ri.RIClosed_Dt != null) ? new Date(ri.RIClosed_Dt.date).toLocaleString('default', { month: 'long' }) : "";
+  //   },
+  //   quartercreated: () => {
+  //       const m = new Date(ri.Created_Ts.date).getMonth();
+  //       return  (m < 3) ? "Q1" : (m < 6) ? "Q2" : (m < 9) ? "Q3" : "Q4";
+  //   },
+  //   quarterclosed: () => {
+  //       const m = (ri.RIClosed_Dt != null) ? new Date(ri.RIClosed_Dt.date).getMonth():"";
+  //       return  (ri.RIClosed_Dt == null) ? "" : (m < 3) ? "Q1" : (m < 6) ? "Q2" : (m < 9) ? "Q3" : "Q4";
+  //   },
+  //   duration: () => {
+  //       const enddate = (ri.RIActive_Flg == 1 || ri.Created_Ts.date < ri.RIClosed_Dt) ? ri.Last_Update_Ts.date : ri.RIClosed_Dt;
+  //       const d = Math.floor((new Date(enddate) - new Date(ri.Created_Ts.date))/(1000 * 60 * 60 * 24));
+  //       return  d + " days";
+  //   },
+  //   Last_Update_Ts: () => {
+  //       return  makestringdate(ri.Last_Update_Ts);
+  //   },
+  //   RIClosed_Dt: () => {
+  //       return  (ri.RIClosed_Dt != null) ? formatDate(new Date(ri.RIClosed_Dt.date)) : "";
+  //   },
+  //   AssociatedCR_Key: () => {
+  //       return  (ri.AssociatedCR_Key) ? "Y" : "N";
+  //   },
+  //   MLMRegion_Cd: () => {
+  //       let list = ""
+  //       let counter = 0;
+  //       for(r of ridata) {
+  //         if (r) {
+  //           if (r.RI_Nm == ri.RI_Nm && r.MLMProgram_Nm == ri.MLMProgram_Nm) {
+  //             counter++;
+  //             list += (!isempty(regions[r.MLMRegion_Cd])) ? regions[r.MLMRegion_Cd] + ", " : (!isempty(regions[r.MLMRegion_Cd])) ? r.MLMRegion_Cd : "";
+  //           }
+  //         }
+  //       }
+  //       return (list.slice(0, -2));
+  //       // return "";
+  //   },
+  //   regioncount: () => {
+  //       let counter = 0;
+  //       for(r of ridata) {
+  //           if (!isempty(r) && r.RI_Nm == ri.RI_Nm && r.MLMProgram_Nm == ri.MLMProgram_Nm) {
+  //           counter++;
+  //           }
+  //       }
+  //       return counter;
+  //   },
+  //   RaidLog_Flg: () => {
+  //       return  (ri.RaidLog_Flg) ? "Y" : "N";
+  //   },
+  //   RiskRealized_Flg: () => {
+  //       return  (ri.RiskRealized_Flg) ? "Y" : "N";
+  //   },
+  //   RIOpen_Hours: () => {
+  //       return Math.floor(ri.RIOpen_Hours/24) + " days";
+  //   },
+  //   driver: () => {
+  //       return (driverlist[ri.RiskAndIssueLog_Key]) 
+  //       ? (driverlist[ri.RiskAndIssueLog_Key]) 
+  //       ? driverlist[ri.RiskAndIssueLog_Key].Driver_Nm : "" : "";
+  //   },
+  //   projectcount: () => {
+  //       let projects = p4plist[ri.RiskAndIssue_Key + "-" + ri.MLMProgramRI_Key];
+  //       return (projects != undefined && projects.length>0) ? projects.length : "";
+  //   }, 
+  //   subprogram: () => {
+  //     let list = "";
+  //     let prog = sublist[ri.RiskAndIssue_Key];
+  //     if (prog != undefined) {
+  //       for(r of prog) {
+  //         let comma = (list.length > 0) ? ", " : "";
+  //         list += comma + r.SubProgram_Nm ;
+  //       } 
+  //     }
+  //     // console.log(list)
+  //     let ret = (list != "") ? list : ""
+  //     // let ret = (list != "") ? list.slice(0, -2) : ""
+  //     // console.log("ret", ret);
+  //     return ret;
+  //   }, 
+  //   MLMProgram_Nm: () => {
+  //     if (ri.Global_Flg == 1) {
+  //       let programs = "";
+  //       let portprog = (ri.RIActive_Flg) ? portfolioprograms : portfolioprogramsclosed;
+  //       portprog.forEach(o => {
+  //         let comma = (programs != "") ? ", " : "";
+  //         if (o.RiskAndIssue_Key == ri.RiskAndIssue_Key) {
+  //           programs = (programs.indexOf(o.Program_Nm) == -1) ? programs + comma + o.Program_Nm : programs;
+  //         }
+  //       })
+  //       if (programs == "" ) {
+  //         programs = ri.MLMProgram_Nm;
+  //       }
+  //       return (programs);
+  //     } else {
+  //       return ri.MLMProgram_Nm;
+  //     }
+  //   },
+  //   programcount: () => {
+  //       let programs = "";
+  //       let pc = 0;
+  //       portfolioprograms.forEach((o) => {
+  //         let comma = (programs != "") ? ", " : ""
+  //         if (o.RiskAndIssue_Key == ri.RiskAndIssue_Key 
+  //           && programs.indexOf(o.MLMProgram_Nm) == -1 && programs.indexOf(o.Program_Nm) == -1) {
+  //             // console.log(o);
+  //           programs = (programs.indexOf(o.Program_Nm) == -1) ? programs + comma + o.Program_Nm : programs;
+  //           // programs = programs + comma + o.Program_Nm;
+  //           pc++;
+  //         } 
+  //       });
+  //       if (ri.RiskAndIssue_Key == 2181) 
+  //         console.log("pc", programs);
+  //       if (pc == 0) {
+  //         pc = 1;
+  //       }
+  //       return (pc);
+  //   },
+  //   age: () => {
+  //     let r = (aplist[ri.RiskAndIssue_Key]) ? new Date(aplist[ri.RiskAndIssue_Key].LastUpdate.date) : "";
+  //     let d = (r == "") ? "" : (Math.floor((new Date() - r)/(1000 * 60 * 60 * 24))+1) ;
+  //     let s = (d == 1) ? " day" : (d == "") ? "" : " days";
+  //     return  `${d}${s}`;
+  //   },
+  //   RIDescription_Txt: () => {
+  //     // return ri.RIDescription_Txt
+  //     return trimmer(ri.RIDescription_Txt, ri.RiskAndIssue_Key, "desc");
+  //   },
+  //   ActionPlanStatus_Cd: () => {
+  //     // return ri.ActionPlanStatus_Cd;
+  //     return trimmer(ri.ActionPlanStatus_Cd, ri.RiskAndIssue_Key, "plan");
+  //   },
+  //   actionplandate: () => {
+  //     let r = (aplist[ri.RiskAndIssue_Key]) ? formatDate(new Date(aplist[ri.RiskAndIssue_Key].LastUpdate.date)) : "";
+  //     return(r);
+  //   }
+  // };
